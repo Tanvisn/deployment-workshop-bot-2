de98681847d75a38a362db5d50e755f7731a9d85
Tanvi Narsapur <59600878+Tanvisn@users.noreply.github.com>
2021-04-04 20:41:51 +0530

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -4,22 +4,22 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
-from typing import Any, Text, Dict, List
+# from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionHi(Action):
+# class ActionHi(Action):
 
-    def name(self) -> Text:
-        return "action_hi"
+#     def name(self) -> Text:
+#         return "action_hi"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Hi, from custom action 'action_hi' !")
-        dispatcher.utter_message(text="How are you?")
+#         dispatcher.utter_message(text="Hi, from custom action 'action_hi' !")
+#         dispatcher.utter_message(text="How are you?")
 
-        return []
+#         return []
